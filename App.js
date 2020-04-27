49c70a3e8d313a7c10ae6bab29a1eb5aa2999790
hiagopsilva <hiagohps360@gmail.com>
2020-04-27 01:28:15 -0300

    add config status bar

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 import Routes from './src/routes';
 
 export default function App() {
-  return <Routes />
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+      <Routes />
+    </>
+  )
 }
 
