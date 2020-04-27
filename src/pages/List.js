298dafc6d32535f802f536aba2173d285f381cd3
hiagopsilva <hiagohps360@gmail.com>
2020-04-27 00:22:35 -0300

    add jsx

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { SafeAreaView, Text, Image, AsyncStorage, StyleSheet } from 'react-native';
 
 // import { Container } from './styles';
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
@@ -14,6 +17,27 @@ export default function List() {
     })
   }, []);
   return (
-    <Text>{techs}</Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: 'center',
+    marginTop: 10
+  },
+
+  list: {
+
+  }
+})
